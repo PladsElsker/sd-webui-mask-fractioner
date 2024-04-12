9152d4a3939264145d352ccf6b748e18a47abc11
JohnWL <34081873+John-WL@users.noreply.github.com>
2024-04-12 14:20:56 -0400

    Bump sdwi2iextender version for Forge support

--- install.py
+++ install.py
@@ -6,10 +6,10 @@ if not launch.is_installed('rectpack'):
     launch.run_pip(f'install rectpack', f"rectpack for sd-webui-mask-fractioner")
 
 
-minimum_sdwi2iextender_version = "0.1.1"
+minimum_sdwi2iextender_version = "0.1.2"
 if not launch.is_installed('sdwi2iextender'):
-    launch.run_pip(f'install sdwi2iextender', f"sdwi2iextender for sd-webui-inpaint-difference")
+    launch.run_pip(f'install sdwi2iextender', f"sdwi2iextender for sd-webui-mask-fractioner")
 else:
     current_version = pkg_resources.get_distribution("sdwi2iextender").version
     if pkg_resources.parse_version(current_version) < pkg_resources.parse_version(minimum_sdwi2iextender_version):
-        launch.run_pip(f'install sdwi2iextender=={minimum_sdwi2iextender_version}', f"sdwi2iextender=={minimum_sdwi2iextender_version} for sd-webui-inpaint-difference")
+        launch.run_pip(f'install sdwi2iextender=={minimum_sdwi2iextender_version}', f"sdwi2iextender=={minimum_sdwi2iextender_version} for sd-webui-mask-fractioner")
